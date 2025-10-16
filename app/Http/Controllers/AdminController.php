ab32bfb9271d8a231895a3f020498d4054909002
19010853 <khoinguyenminhk37@gmail.com>
2025-10-16 08:00:42 +0700

    set up admin panel and add logout option

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate(); 
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+    // End Method 
+
+    public function AdminProfile(){
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile',compact('profileData'));
+
+    }
+     // End Method 
+
+    
+
+}
