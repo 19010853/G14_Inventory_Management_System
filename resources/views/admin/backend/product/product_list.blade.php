f7e0fbfe87acddd5fc87644de21bd86e3b1def8f
19010853 <khoinguyenminhk37@gmail.com>
2025-10-20 18:56:38 +0700

    add editing product method

--- resources/views/admin/backend/product/product_list.blade.php
+++ resources/views/admin/backend/product/product_list.blade.php
@@ -73,17 +73,28 @@
                       </td>
                       <td>
                         <a
+                          title="Details"
                           href="{{ route('edit.customer', $item->id) }}"
+                          class="btn btn-info btn-sm"
+                        >
+                          <span class="mdi mdi-eye-circle mdi-18px"></span>
+                        </a>
+
+                        <a
+                          title="Edit"
+                          href="{{ route('edit.product', $item->id) }}"
                           class="btn btn-success btn-sm"
                         >
-                          Edit
+                          <span class="mdi mdi-book-edit mdi-18px"></span>
                         </a>
+
                         <a
-                          href="{{ route('delete.customer', $item->id) }}"
+                          title="Delete"
+                          href="{{ route('delete.product', $item->id) }}"
                           class="btn btn-danger btn-sm"
                           id="delete"
                         >
-                          Delete
+                          <span class="mdi mdi-delete-circle mdi-18px"></span>
                         </a>
                       </td>
                     </tr>
