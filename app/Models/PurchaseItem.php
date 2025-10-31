4afafb5a1347e4d39e53f2ac9e53a92504843f8a
19010853 <khoinguyenminhk37@gmail.com>
2025-10-31 23:21:59 +0700

    create relationship for purchase and purchase items

--- app/Models/PurchaseItem.php
+++ app/Models/PurchaseItem.php
@@ -8,4 +8,12 @@ class PurchaseItem extends Model
 {
     //
     protected $guarded = [];
+
+    public function purchase(): BelongsTo{
+        return $this->belongsTo(Purchase::class, 'purchase_id');
+    }
+
+    public function product(): BelongsTo{
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
